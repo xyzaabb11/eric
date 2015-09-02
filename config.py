1836a2f613677a05f7dd320042d45a3e26afd774
yjamje <finallym@hotmail.com>
2015-09-02 16:11:39 +0800

    增加登录模板和视图

---
+++ config.py
@@ -0,0 +1,8 @@
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-nerver-guess'
+
+OPENID_PROVIDERS = [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+	{'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+	
+]
